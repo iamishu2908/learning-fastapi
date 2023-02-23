433f12479d63388335ebca714429abef15664994
ishud <ishudev2021@gmail.com>
2023-02-23 22:38:53 +0530

    summary and description

--- main.py
+++ main.py
@@ -11,40 +11,47 @@ app = FastAPI()
 def index():
     return {'message' : "Hello World !"}
 
-# return status code on response as well
-@app.get('/blog/{id}', status_code= status.HTTP_404_NOT_FOUND, tags=['blog'])
-def get_blog(id : int,response:Response):
-    if id > 100 :
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {'error' : f'Blog {id} not found XO'}
-    else:
-        response.status_code = status.HTTP_200_OK
-        return {'message' : f'blog with id {id} found succesfully!!!'}
+# summary and description
+@app.get('/blog/all',tags = ['blog'], summary = 'Retrieve all blogs', description = 'this api fetches all blogs.')
+def get_all_blogs(page=1):
+    return {'message' : f'it has {page} pages.'}
     
-    
-#query and path parameters
+# description as docstring
 @app.get('/blog/{id}/comments/{comment_id}',tags = ['blog','comment'])
 def get_comment(id:int,comment_id:int,valid:bool=True,username:Optional[str] = None):
+    """
+    Simulates retrieving a comment of a blog
+    - **id** mandatory path parameter
+    - **comment_id** mandatory path parameter
+    - **bool** optional query parameter
+    - **username** optional query parameter
+    """
     return {'message' : f'blog_id-> {id}, comment_id -> {comment_id}, valid : {valid}, username:{username}'}
 
-#optional parameters
 
-@app.get('/blog/all')
-def get_blogs(page = 1, pagesize : Optional[int] = None):
-    return {'message' : f'There are {pagesize} pages on page {page}'}
-#query parameters(defined values)
+# #query and path parameters
+# @app.get('/blog/{id}/comments/{comment_id}',tags = ['blog','comment'])
+# def get_comment(id:int,comment_id:int,valid:bool=True,username:Optional[str] = None):
+#     return {'message' : f'blog_id-> {id}, comment_id -> {comment_id}, valid : {valid}, username:{username}'}
 
-@app.get('/blog/all')
-def get_blogs(page = 1, pagesize = 10):
-    return {'message' : f'There are {pagesize} pages on page {page}'}
+# #optional parameters
 
-# predefined values
-class BlogType(str,Enum) :
-    short = 'short'
-    story = 'story'
-    howto = 'howto'
+# @app.get('/blog/all')
+# def get_blogs(page = 1, pagesize : Optional[int] = None):
+#     return {'message' : f'There are {pagesize} pages on page {page}'}
+# #query parameters(defined values)
 
-@app.get('/blog/type/{type}')
-def get_blog_type(type:BlogType):
-    return {'message' : f'Blog type {type}'}
+# @app.get('/blog/all')
+# def get_blogs(page = 1, pagesize = 10):
+#     return {'message' : f'There are {pagesize} pages on page {page}'}
+
+# # predefined values
+# class BlogType(str,Enum) :
+#     short = 'short'
+#     story = 'story'
+#     howto = 'howto'
+
+# @app.get('/blog/type/{type}')
+# def get_blog_type(type:BlogType):
+#     return {'message' : f'Blog type {type}'}
 
