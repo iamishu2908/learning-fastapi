0c4d59a0c8f67ceb3d6dc530c86746386ff29c63
ishud <ishudev2021@gmail.com>
2023-02-23 22:25:55 +0530

    tags

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def index():
     return {'message' : "Hello World !"}
 
 # return status code on response as well
-@app.get('/blog/{id}', status_code= status.HTTP_404_NOT_FOUND)
+@app.get('/blog/{id}', status_code= status.HTTP_404_NOT_FOUND, tags=['blog'])
 def get_blog(id : int,response:Response):
     if id > 100 :
         response.status_code = status.HTTP_404_NOT_FOUND
@@ -20,8 +20,10 @@ def get_blog(id : int,response:Response):
     else:
         response.status_code = status.HTTP_200_OK
         return {'message' : f'blog with id {id} found succesfully!!!'}
+    
+    
 #query and path parameters
-@app.get('/blog/{id}/comments/{comment_id}')
+@app.get('/blog/{id}/comments/{comment_id}',tags = ['blog','comment'])
 def get_comment(id:int,comment_id:int,valid:bool=True,username:Optional[str] = None):
     return {'message' : f'blog_id-> {id}, comment_id -> {comment_id}, valid : {valid}, username:{username}'}
 
