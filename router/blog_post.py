c5dced478a61bc3be7df39baf9d4cd9f7afc8473
ishud <ishudev2021@gmail.com>
2023-02-24 21:42:47 +0530

    multiple values(list)

--- router/blog_post.py
+++ router/blog_post.py
@@ -1,4 +1,4 @@
-from typing import Optional
+from typing import Optional,List
 from fastapi import APIRouter, Query,Body
 from pydantic import BaseModel
 
@@ -21,10 +21,23 @@ def create_blog(blog : BlogModel, id:int, version : int =1):
 
 @router.post('/new/{id}/comment')
 def create_comment(blog: BlogModel, id: int, 
-                   comment_id : int = Query(None,title='Id of the comment',description = 'some desc',alias = 'CommentID',deprecated = True),content:str = Body(...,min_length=10,max_length=12,regex='^[a-z\s]*$')):
+                   comment_id : int = Query(
+    None,
+    title='Id of the comment',
+    description = 'some desc',
+    alias = 'CommentID',deprecated = True),
+    content:str = Body(
+        ...,
+        min_length=10,
+        max_length=12,
+        regex='^[a-z\s]*$'),
+    v : Optional[List[str]] = Query(None)
+    ):
+
     return {
         'blog' : blog,
         'id' : id,
         'comment_id' : comment_id,
-        'content' : content 
+        'content' : content ,
+        'version' : v
     }
