a8dc177f428347f37431c2df269ca9f00adfa71c
ishud <ishudev2021@gmail.com>
2023-02-24 14:58:33 +0530

    adding second routers-get and post

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from router import blog_get
+from router import blog_post
 
 # we use this line to start our server and to define our path(@app.get)
 app = FastAPI()
 app.include_router(blog_get.router)
+app.include_router(blog_post.router)
 
 #attaching function to an endpoint
 @app.get('/')
