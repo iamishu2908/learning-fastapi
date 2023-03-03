277b9cec236e6f3c6d4d15536f8f4da489082051
ishud <ishudev2021@gmail.com>
2023-03-03 14:45:43 +0530

    responses

--- router/product.py
+++ router/product.py
@@ -1,6 +1,6 @@
 
 from fastapi import APIRouter
-from fastapi.responses import Response
+from fastapi.responses import Response,HTMLResponse,PlainTextResponse
 
 
 router = APIRouter(
@@ -15,3 +15,43 @@ def get_all_products():
     #return products
     data = " ".join(products)
     return Response(content=data, media_type = "text/plain")
+
+@router.get('/{id}', responses = {
+    200:{
+    "content":{
+    "text/html" : {
+    "example":"<div>Product</div>"
+    }
+    },
+    "description" : "returns the html for an object"
+    },404:{
+    "content":{
+    "text/plain" : {
+    "example":"<div>Product not available! </div>"
+    }
+    },
+    "description" : "a cleartext error message"
+    }
+})
+def get_product(id:int):
+    if id > len(products):
+        out = "Product not available"
+        return PlainTextResponse(status_code=404 ,content = out, media_type = "text/plain")
+    else:
+      product = products[id]
+      out = f"""
+      <head>
+        <style>
+        .product {{
+        width:500px;
+        height:30px;
+        border: 2px inset green;
+        background-color: lightblue;
+        text-align: center;
+        }}
+        </style>
+      </head>
+      <div class="product">{product}</div>
+
+      """
+      return HTMLResponse(content=out,media_type="text/html")
