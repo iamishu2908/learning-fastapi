eb4d5cd9396f5ea1e22452f8ca2a2873ed250d6d
ishud <ishudev2021@gmail.com>
2023-03-04 23:48:36 +0530

    user authentication

--- auth/oauth2.py
+++ auth/oauth2.py
@@ -1,7 +1,14 @@
+from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer
 from typing import Optional
 from datetime import datetime, timedelta
 from jose import jwt
+from sqlalchemy.orm.session import Session
+from db.database import get_db
+from fastapi import HTTPException,status
+from jose.exceptions import JWTError
+
+from db import db_user
  
  
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
@@ -19,3 +26,21 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
   to_encode.update({"exp": expire})
   encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
   return encoded_jwt
+
+def get_current_user(toke:str = Depends(oauth2_scheme), db:Session = Depends(get_db)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail='could not validate credentials!',headers = {'WWW-Authenticate' : 'Bearer'})
+    try:
+        payload = jwt.decode(toke,SECRET_KEY,algorithms=[ALGORITHM])
+        username : str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+    except JWTError:
+       raise credentials_exception
+    
+    user = db_user.get_user_by_username(db,username)
+
+    if user is None:
+       raise credentials_exception
+    
+    return user
+       
--- router/user.py
+++ router/user.py
@@ -4,6 +4,7 @@ from db.database import get_db
 from schemas import UserBase, UserDisplay
 from sqlalchemy.orm import Session
 from db import db_user
+from auth.oauth2 import get_current_user
 
 router = APIRouter(
     prefix = '/user',
@@ -17,21 +18,21 @@ def create_user(request: UserBase, db : Session = Depends(get_db)):
 
 # read all users
 @router.get('/', response_model = List[UserDisplay])
-def get_all_users(db:Session = Depends(get_db)):
+def get_all_users(db:Session = Depends(get_db), current_user: UserBase = Depends(get_current_user)):
     return db_user.get_all_users(db)
 
 #read one user by id
 @router.get('/{id}',response_model = UserDisplay)
-def get_user(id:int,db:Session=Depends(get_db)):
+def get_user(id:int,db:Session=Depends(get_db), current_user: UserBase = Depends(get_current_user)):
     return db_user.get_user(db,id)
 
 
 # update user
 @router.post('/{id}/update')
-def update_user(id:int,request:UserBase,db:Session=Depends(get_db)):
+def update_user(id:int,request:UserBase,db:Session=Depends(get_db), current_user: UserBase = Depends(get_current_user)):
     return db_user.update_user(db,id,request)
 
 # delete user
 @router.get('/{id}/delete')
-def delete_user(id:int,db:Session=Depends(get_db)):
+def delete_user(id:int,db:Session=Depends(get_db), current_user: UserBase = Depends(get_current_user)):
     return db_user.delete_user(db,id)
